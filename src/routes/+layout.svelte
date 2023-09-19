beb4973680f5b48b8c08160f138263cc49998528
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2023-09-19 13:29:29 +0200

    [feat]SSR-Routing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,6 @@
 	import Footer from '../components/Footer.svelte';
 	import Header from '../components/Header.svelte';
 	import Tabs from '../shared/Tabs.svelte';
-	import './styles.css';
 	const tabs = [
 		{ title: 'Current Polls', link: '/' },
 		{ title: 'Add a new Poll', link: '/create' }
