a23278926f8464d198398c526240c732fcb1c69b
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2023-09-21 11:16:00 +0200

    prisma init

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,30 @@
 <script>
 	// @ts-nocheck
-	import "./globals.css"
+	import './globals.css';
 	import Footer from '../components/Footer.svelte';
 	import Header from '../components/Header.svelte';
 	import Tabs from '../shared/Tabs.svelte';
-	import { tweened } from "svelte/motion";
+	import { tweened } from 'svelte/motion';
 	const tabs = [
 		{ title: 'Current Polls', link: '/' },
 		{ title: 'Add a new Poll', link: '/create' }
 	];
 	const value = tweened(0);
-
 </script>
+
 <div class="app">
 	<Header />
 	<main>
-		<Tabs  {tabs} />
+		<Tabs {tabs} />
 		<slot />
 	</main>
 	<Footer />
 </div>
 
 <style>
+	main {
+		max-width: 1800px;
+		padding-inline: 20px;
+		margin: 0 auto;
+	}
 </style>
